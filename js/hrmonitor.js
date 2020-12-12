f5e0100a0f3e123d8d02b5e831ffdd7c941b4cb5
Volodymyr <volodymyr.morgunov@progresafinal.sk>
2020-12-12 19:35:55 +0100

    9hrm

--- js/hrmonitor.js
+++ js/hrmonitor.js
@@ -1,5 +1,6 @@
 console.log("hrmonitor.js")
 
+
 var options = 
 {
     filters:
@@ -9,8 +10,41 @@ var options =
         }
     ]
 }
+
+parseValue = (value) => {
+    // В Chrome 50+ используется DataView.
+    value = value.buffer ? value : new DataView(value);
+    let flags = value.getUint8(0);
+
+    // Определяем формат
+    let rate16Bits = flags & 0x1;
+    let result = {};
+    let index = 1;
+
+    // Читаем в зависимости от типа
+    if (rate16Bits) {
+      result.heartRate = value.getUint16(index, /*littleEndian=*/true);
+      index += 2;
+    } else {
+      result.heartRate = value.getUint8(index);
+      index += 1;
+    }
+
+    // RR интервалы
+    let rrIntervalPresent = flags & 0x10;
+    if (rrIntervalPresent) {
+      let rrIntervals = [];
+      for (; index + 1 < value.byteLength; index += 2) {
+        rrIntervals.push(value.getUint16(index, /*littleEndian=*/true));
+      }
+      result.rrIntervals = rrIntervals;
+    }
+
+    return result;
+  }
+
 var deviceInfo = document.getElementById('Device')
-deviceInfo.innerText = "Device #8th"
+deviceInfo.innerText = "Device #9th"
 function SearchBT()
 {
     //deviceInfo.innerText +=" CLicked\n";
