e1ef5d3bb2c569351e0234828258c407046fefab
Volodymyr <volodymyr.morgunov@progresafinal.sk>
2020-12-12 17:37:12 +0100

    sw

--- js/hrmonitor.js
+++ js/hrmonitor.js
@@ -17,7 +17,7 @@ navigator.bluetooth.requestDevice({filters:[{services:[ 'heart_rate' ]}]}) //opt
     deviceInfo.innerText += "inside bluetooth";//device.name;
     console.log('> Id:               ' + device.id);
     console.log('> Connected:        ' + device.gatt.connected);
-  })
+  }).then(function(device){return device.gatt.connect})
   .catch(error => {
     console.log('Argh! ' + error);
   });
