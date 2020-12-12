a303de8b0fa3bcac66fd2768b7351654ae9b83b7
Volodymyr <volodymyr.morgunov@progresafinal.sk>
2020-12-12 16:44:33 +0100

    bluetouth

--- js/hrmonitor.js
+++ js/hrmonitor.js
@@ -10,11 +10,11 @@ console.log("hrmonitor.js")
     ]
 }*/
 var deviceInfo = document.getElementById('Device')
-deviceInfo.innerText = "Device #1s"
+deviceInfo.innerText = "Device #2nd"
 navigator.bluetooth.requestDevice({filters:[{services:[ 'heart_rate' ]}]}) //options
   .then(device => {
     log('> Name:             ' + device.name);
-    deviceInfo.innerText = device.name;
+    deviceInfo.innerText += "inside bluetooth";//device.name;
     log('> Id:               ' + device.id);
     log('> Connected:        ' + device.gatt.connected);
   })
