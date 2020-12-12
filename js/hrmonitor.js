499b08d580f3f5e1f8429ebfa0476cb0f9d45b64
Volodymyr <volodymyr.morgunov@progresafinal.sk>
2020-12-12 16:24:34 +0100

    deiceName

--- js/hrmonitor.js
+++ js/hrmonitor.js
@@ -9,12 +9,16 @@ console.log("hrmonitor.js")
         }
     ]
 }*/
+var deviceInfo = document.getElementById('Device')
+deviceInfo.innerText = "Device"
 navigator.bluetooth.requestDevice({filters:[{services:[ 'heart_rate' ]}]}) //options
   .then(device => {
     log('> Name:             ' + device.name);
+    deviceInfo.innerText += device.name;
     log('> Id:               ' + device.id);
     log('> Connected:        ' + device.gatt.connected);
   })
   .catch(error => {
     log('Argh! ' + error);
   });
+
